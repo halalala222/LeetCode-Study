ab6daf479ab24481e82125a2aa23b6dfce72075a
halalala222 <1741196223@qq.com>
2022-11-02 23:11:22 +0800

    (283) 2022.11.2 Day10

---
+++ 283.移动零.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=283 lang=cpp
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
+// @lc code=end
+
