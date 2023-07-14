9b1e6cb27d6301849981198e9041aa0867be0115
halalala222 <1741196223@qq.com>
2023-07-14 14:23:25 +0800

    2023.7.14 979

---
+++ 979.在二叉树中分配硬币.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=979 lang=java
+ *
+ * [979] 在二叉树中分配硬币
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * 这是一个从下至上的过程。我觉得需要从根节点向上反推会更好的思考这个过程   
+ */
+class Solution {
+    private int ans;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans += Math.abs(left) + Math.abs(right);
+        return ans + root.val - 1;
+    }
+}
+// @lc code=end
